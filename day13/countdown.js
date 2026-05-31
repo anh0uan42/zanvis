cb3ca47c8a572d4c44160512d4011d86aa4ea50a
Q Nguyen <nguyenanhquancttpcl@gmail.com>
2026-06-01 01:23:36 +0700

    day 13 fixed

--- day13/countdown.js
+++
@@ -1,8 +0,0 @@
-let count = 5;
-
-while (count >= 0) {
-    console.log(count);
-    count--; // Subtracts 1 from count during each iteration
-}
-
-console.log("Blastoff!");
--- day13/tasks.js
+++ day13/tasks.js
@@ -16,3 +16,44 @@ const addTwo = num => {
 greet('Ben', 'Basic HTML')
 sayName('John')
 console.log(`Add 2 to 7 give us a ${addTwo(7)}`)
+
+//task 5
+for (let i = 1; i <= 50; i++) {
+  console.log(i);
+}
+
+//task 6
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i)
+  }
+}
+
+//task 7
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i)
+  }
+}
+
+//task 8
+const num = 5;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${num} x ${i} = ${num * i}`);
+}
+
+//task9
+let second = 10
+
+while (second > 0) {
+    console.log(`There is ${second} more seconds to New Year`)
+    second--;
+}
+
+//task 10
+let count = 1
+do {
+  console.log(`This will run from 1 to 10. This is the ${count} round`)
+  count++;
+} while (count <= 10)
